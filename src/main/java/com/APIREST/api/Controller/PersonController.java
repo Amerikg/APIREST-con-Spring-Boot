06cc955b6691d6027b5c2bbd234e853f4efcb633
Amerikg <amelizruiz@gmail.com>
2025-04-03 10:09:34 -0600

    Primer commit: Subiendo la API REST de Spring Boot

---
+++ src/main/java/com/APIREST/api/Controller/PersonController.java
@@ -0,0 +1,53 @@
+package com.APIREST.api.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.APIREST.api.model.Person;
+import com.APIREST.api.repository.PersonRepository;
+
+@RestController
+@RequestMapping("/api")
+public class PersonController {
+
+    @Autowired
+    private PersonRepository repository;
+
+    @GetMapping("/persons")
+    public List<Person> allPersons(){
+        return repository.findAll();
+    }
+    
+    @GetMapping("/person/{name}")
+    public List<Person> findByName(@PathVariable("name") String name){
+        return repository.findByName(name);
+    }
+
+    @PostMapping("/person")
+    public Person creaPerson(@RequestBody Person person){
+        return repository.save(person);
+    }
+
+    @PutMapping("/person/{id}")
+    public Person updatPerson(@PathVariable int id, @RequestBody Person person){
+        return repository.save(person);
+    }
+
+    @DeleteMapping("/person/{id}")
+    public void deletePerson(@PathVariable("id") Long id){
+        repository.deleteById(id);
+    }
+
+    
+
+
+}
---
+++ src/main/java/com/APIREST/api/repository/PersonRepository.java
@@ -0,0 +1,15 @@
+package com.APIREST.api.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.APIREST.api.model.Person;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long>{
+
+    List<Person> findByName(@Param("name") String name);
+} 
